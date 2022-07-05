5e4022a81191f3fc4c3c6c892d8fbe094d425d3c
everson-junior <45975050+everson-junior@users.noreply.github.com>
2022-07-05 14:55:36 -0300

    Delete src/app/sample-po-page-dynamic-table-basic/sample-po-page-dynamic-table-basic directory

--- src/app/sample-po-page-dynamic-table-basic/sample-po-page-dynamic-table-basic/sample-po-page-dynamic-table-basic.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sample-po-page-dynamic-table-basic',
-  templateUrl: './sample-po-page-dynamic-table-basic.component.html'
-})
-export class SamplePoPageDynamicTableBasicComponent {}
